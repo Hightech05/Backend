5286fa5196d920dcc8c68d13eef77ee95c944283
Divy-0503 <divypatel0503@gmail.com>
2024-06-16 14:57:22 +0530

    routes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -252,7 +252,7 @@ const changeCurrentPassword = asyncHandler(async(req, res)=> {
 const getCurrentUser = asyncHandler(async(req, res)=> {
     return res 
     .status(200)
-    .json(200, req.user, "current user fetched successsfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successsfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res)=> {
@@ -262,7 +262,7 @@ const updateAccountDetails = asyncHandler(async(req, res)=> {
         throw new ApiError(400, "All field is required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -284,13 +284,29 @@ const updateUserAvatar = asyncHandler(async(req, res)=> {
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is missing")
     }
+// todo: delete old image - assignment
+    
+    // Fetch the current user to get the old avatar URL
+    const oldavatar = await User.findById(req.user?._id).select("avatar");
 
-    const avatar = awaituploadOnCloudinary(avatarLocalPath)
+    if (oldavatar.avatar) {
+        // Extract public ID of the old image from the URL
+        const oldAvatarPublicId = oldavatar.avatar.split('/').pop().split('.')[0];
+
+        // Delete the old image from Cloudinary
+        try {
+            await cloudinary.uploader.destroy(oldAvatarPublicId);
+        } catch (error) {
+            throw new ApiError(500, "Failed to delete old avatar image");
+        }
+    }
+// end delete
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
     if (!avatar.url) {
         throw new ApiError(400, "Error while uploadinf avatar")
     }
 
-    const user =await User.findByIdAndDelete(
+    const user =await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -311,12 +327,27 @@ const updateUserCoverImage = asyncHandler(async(req, res)=> {
         throw new ApiError(400, "Cover image is missing")
     }
 
-    const coverImage = awaituploadOnCloudinary(avatarLocalPath)
+    // coverimage delete 
+    const oldcoverImage = await User.findById(req.user?._id).select("coverImage");
+
+    if (oldcoverImage.coverImage) {
+        // Extract public ID of the old image from the URL
+        const oldCoverImagePublicId = user.coverImage.split('/').pop().split('.')[0];
+
+        // Delete the old image from Cloudinary
+        try {
+            await cloudinary.uploader.destroy(oldCoverImagePublicId);
+        } catch (error) {
+            throw new ApiError(500, "Failed to delete old cover image");
+        }
+    }
+// end delete
+    const coverImage = await uploadOnCloudinary(avatarLocalPath)
     if (!coverImage.url) {
         throw new ApiError(400, "Error while uploadinf avatar")
     }
-
-    const user = await User.findByIdAndDelete(
+    
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -330,6 +361,123 @@ const updateUserCoverImage = asyncHandler(async(req, res)=> {
     .json(new ApiResponse(200, user , "Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async(req, res)=> {
+    const {username} = req.params
+     
+    if (!username?.trim()) {
+        throw new ApiError(400, "usernmae is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in: [req.user?._id,"subscribers"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },{
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+
+            }
+        }
+    ])
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "user channel fetched successfuy"))
+})
+
+const getWatchHistory = asyncHandler(async(req, res)=> {
+    const user =await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"user",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user[0].getWatchHistory, "watch history fetched successfully"))
+})
+
+
 export {
     registerUser, 
     loginUser,
@@ -339,7 +487,9 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
 }
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -50,7 +50,19 @@
 // export default router
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refereshAccessToken } from "../controllers/user.controllers.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refereshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -76,6 +88,15 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refereshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("cover-image"),updateUserCoverImage )
+
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 export default router
