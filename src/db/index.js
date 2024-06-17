f8cdd28cd8f21442846f45a119136c02bbd25c57
Divy-0503 <divypatel0503@gmail.com>
2024-06-17 10:44:41 +0530

    assignment file

--- src/db/index.js
+++ src/db/index.js
@@ -14,19 +14,4 @@ const connectDB = async () => {
 
 export default connectDB
 
-// import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
 
-
-// const connectDB = async()=>{
-//     try{
-//        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//        console.log(`\n MongoDb connected !! DB HOSST : ${connectionInstance.connection.host}`);
-//     }catch(error){
-//         console.log("MONGODB connection error", error);
-//         process.exit(1)
-//     }
-
-// }
-
-// export default connectDB
